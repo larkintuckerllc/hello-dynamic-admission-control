add23df798cec49e38c8a437868dbe45ec8effcf
John Tucker <john@larkintuckerllc.com>
2020-09-10 14:25:30 -0400

    operational

--- app/app.js
+++ app/app.js
@@ -1,12 +1,41 @@
+const bodyParser = require('body-parser');
 const express = require('express');
+const fs = require('fs');
+const https = require('https');
 
 const app = express();
-const port = 3000;
+app.use(bodyParser.json());
+const port = 443;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+const options = { 
+  ca: fs.readFileSync('ca.crt'), 
+  cert: fs.readFileSync('server.crt'), 
+  key: fs.readFileSync('server.key'), 
+}; 
+
+app.post('/', (req, res) => {
+  if (
+    req.body.request === undefined ||
+    req.body.request.uid === undefined
+  ) {
+    res.status(400).send();
+    return;
+  }
+  console.log(req.body); // DEBUGGING
+  const { request: { uid } } = req.body;
+  res.send({
+    apiVersion: 'admission.k8s.io/v1',
+    kind: 'AdmissionReview',
+    response: {
+      uid,
+      allowed: true,
+    },
+  });
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+const server = https.createServer(options, app);
+
+server.listen(port, () => {
+  console.log(`Server running on port ${port}/`);
 });
+
